20319637521fb72ff9f8dbee6a3e0e5fa69dad69
Bryan <bryanjmejia11@gamil.com>
2026-02-27 18:07:39 -0800

    Add Restaurant Info assignment

---
+++ RestaurantInfo.java
@@ -0,0 +1,10 @@
+public class RestaurantInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Restaurant Name: Dog House Grill");
+        System.out.println("Address: 2789 E Shaw Ave");
+        System.out.println("City, State, Zip: Fresno, CA 93710");
+
+    }
+}
